acfe1a78599aef8203c000930a498a81328a93d2
Lord-sarcastic <adeoti.15.jude@gmail.com>
2020-08-04 01:38:27 +0100

    Chore: Made class variables private to prevent unworthy manipulation, added methods to retrieve instance variables

--- validator/validator.py
+++ validator/validator.py
@@ -1,25 +1,32 @@
 from re import search
 
+
 class Validator:
 
     def __init__(self, username, password):
-        self.username = username
-        self.password = password
+        self._username = username
+        self._password = password
         
     def username_in_password(self):
-        return search(self.username, self.password)
+        return search(self._username, self._password)
 
     def password_is_common(self):
         with open("common_passwords.txt", "r") as c:
-            return self.password in c.read()
+            return self._password in c.read()
     
     def is_minimum_length(self, min_length):
-        return self.password >= min_length
+        return self._password >= min_length
 
     def contains_alphanumeric_and_symbols(self):
-        contains_num_and_letter = search("[A-Za-z0-9]+", self.password)
-        contains_special_character = search("[!@#$%^&*]+", self.password)
+        contains_num_and_letter = search("[A-Za-z0-9]+", self._password)
+        contains_special_character = search("[!@#$%^&*]+", self._password)
         return contains_num_and_letter and contains_special_character
         
     def confirm_password(self, password2):
-        return self.password == password2
+        return self._password == password2
+
+    def get_password(self):
+        return self._password
+
+    def get_username(self):
+        return self._username
